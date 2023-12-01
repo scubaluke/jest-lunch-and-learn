fb82be73d9d6c4aad889b2b269d776f68b6fa0f8
luke gartland <lukegartland@lukes-MBP-2.lan>
2023-12-01 10:05:05 -0600

    add route validation

--- starterCode/src/handlers/rest.handler.ts
+++ starterCode/src/handlers/rest.handler.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 import { add } from '../helpers/add'
+import { User } from '../dataSource'
 
 export class RestHandler {
   public static async respond(req: Request, res: Response, next: NextFunction) {
@@ -27,6 +28,16 @@ export class RestHandler {
         .map((number) => Number(number.trim()))
       const sum = add(numbersArray)
       res.status(200).json({ sum })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public static async users(req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await User.findAll()
+
+      res.status(200).json({ users })
     } catch (error) {
       next(error)
     }
---
+++ starterCode/src/middleware/validateQuery.ts
@@ -0,0 +1,53 @@
+import express from 'express'
+
+import {
+  ContextRunner,
+  ValidationChain,
+  validationResult,
+  query,
+} from 'express-validator'
+
+// can be reused by many routes
+export function validate(validations: ContextRunner[]) {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    for (const validation of validations) {
+      await validation.run(req)
+    }
+
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
+
+    res.status(400).json({
+      message: 'please fixe validation errors',
+      errors: errors.array(),
+    })
+  }
+}
+
+export function validateNumericArray(): ValidationChain[] {
+  return [
+    query('numbers')
+      .notEmpty()
+      .custom((numbers: string) => {
+        const numbersArray = numbers
+          .split(',')
+          .map((number) => Number(number.trim()))
+        return numbersArray.every((number) => !isNaN(number))
+      }),
+  ]
+}
+
+export function validateDateFormat(): ValidationChain[] {
+  return [
+    query('date-format')
+      .optional()
+      .toUpperCase()
+      .isIn(['YYYY-MM-DD', 'DD-MM-YY', 'MM-DD-YY']),
+  ]
+}
